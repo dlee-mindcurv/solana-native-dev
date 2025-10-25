e27bb1dbf10592f56777c7bc5f1ea589811cc33b
david.lee <david.lee@mindcurv.com>
2025-10-25 03:49:21 +0200

    **Add initial Solana program tests and setup initial dependencies**

---
+++ hello-solana/program/src/lib.rs
@@ -0,0 +1,21 @@
+use solana_program::account_info::AccountInfo;
+use solana_program::{entrypoint, msg};
+use solana_program::entrypoint::ProgramResult;
+use solana_program::pubkey::Pubkey;
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8]
+) -> ProgramResult {
+    msg!("Hello, Solana!");
+    msg!("Our program's ID is: {}", program_id);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests;
+
+
